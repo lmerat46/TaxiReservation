532c9512a3024e417013394316b9a76ea01271ea
lmerat46 <merat.laure@hotmail.fr>
2018-03-21 22:41:10 +0100

    restructuration

--- node/node.js
+++ node/node.js
@@ -1,16 +1,33 @@
+/**
+ * required modules
+ */
 var http = require('http');
 var m = require('fs');
 var mysql = require('mysql');
 var express = require('express');
+var body = require('body-parser');
 
 var serv = express();
-
-var body = require('body-parser');
 
 serv.use( body.json() );       // to support JSON-encoded bodies
 serv.use(body.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+
+serv.use(express.static(__dirname + '/public'));
+
+/**
+ * connection to database
+ */
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Mathildemerat1999",
+    database: "taxiReservation"
+  });
+  
+  connection.connect();
+
 
 serv.get('/home', function(req, res) {
     if(req.body.hasOwnProperty("client")){
@@ -21,11 +38,11 @@ serv.get('/home', function(req, res) {
     res.render('/home', { title: 'post' });
   });
 
+
 /**
  * create account Mysql request
  */
 serv.post('/', function(req, res) {
-    console.log(req.body);
     var lastname = req.body.lastname;
     var firstname = req.body.firstname;
     var birthday = req.body.birthday;
@@ -38,19 +55,39 @@ serv.post('/', function(req, res) {
     }else{
         connection.query("INSERT INTO client (lastname, firstname, pseudo, birthday, mail, phone, passwd) VALUES ('lastname', 'firstname', 'pseudo', 'birthday', 'mail', 'phone', 'password')");
     }
-               
+    res.redirect('../html/home.html');
+});
 
-            /*
-        request.on('end', function () {
-            var post = qs.parse(body);
-            // use post['blah'], etc.
-        });
-        */
+/**
+ * check login
+ */
+serv.post('/', function(req,res){
+    var pseudo = req.body.pseudo;
+    var password = req.body.password;
+    if(connection.query("SELECT pseudo, password FROM client WHERE pseudo = 'pseudo' and password = 'password")){
+        /**
+         * TODO connection to client page
+         */
+        res.redirect('../html/client.html');
+
+    }else if(connection.query("SELECT pseudo, password FROM driver WHERE pseudo = 'pseudo' and password = 'password")){
+        /**
+         * TODO connection to driver page
+         */
+        res.redirect('../html/driver.html');
+    }else{
+        /**
+         * TODO throw error and permitte reconnection
+         */
+    }
 });
 
 
+/**
+ * affiche home page
+ */
 serv.get('/',function(req, res) {
-    m.readFile('createAccount.html',function(erreur,donnees){
+    m.readFile('../html/client.html',function(erreur,donnees){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(donnees);
         res.end();
@@ -68,18 +105,4 @@ serv.get('/',function(req, res) {
             res.end();
         });
     });*/
-
-
-
-
-
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Mathildemerat1999",
-    database: "taxiReservation"
-  });
-  
-  connection.connect();
 
